e4fc3dd0ad98efa40ddddb1390f0e0a4951575e7
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-11-06 18:37:37 +0530

    matrix of any size + uses less disk

--- MatrixAdd.java
+++ MatrixAdd.java
@@ -1,45 +1,36 @@
 import java.util.*;
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main (String args[])
-    {
+public class Main {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of rows for the square matrix: ");
-        int row=sc.nextInt();
-        int a[][] = new int [row][row];
-        int b[][] = new int [row][row];
-        int c[][] = new int [row][row];
-        int i,j;
-        for(i=0;i<row;i++)
-        {
-            for(j=0;j<row;j++)
-            {
-               System.out.println("Enter elements of the first matrix row wise: ");
-               a[i][j]=sc.nextInt();
+        System.out.println("Enter number of row for the matrix:");
+        int row = sc.nextInt();
+        System.out.println("Enter number of col fo2r the matrix:");
+        int col = sc.nextInt();
+        int a[][] = new int[row][col];
+        int b[][] = new int[row][col];
+        int i, j;
+        for (i = 0; i < row; i++) {
+            for (j = 0; j < col; j++) {
+                System.out.println("Enter the element a" + (i + 1) + (j + 1));
+                a[i][j] = sc.nextInt();
             }
         }
-        for(i=0;i<row;i++)
-        {
-            for(j=0;j<row;j++)
-            {
-               System.out.println("Enter elements of the second matrix row wise: ");
-               b[i][j]=sc.nextInt();
+        for (i = 0; i < row; i++) {
+            for (j = 0; j < col; j++) {
+                System.out.println("Enter the element b" + (i + 1) + (j + 1));
+                b[i][j] = sc.nextInt();
             }
         }
-        
+
         System.out.println("Addition of the two matrix: ");
-        for(i=0;i<row;i++)
-        {
-            for(j=0;j<row;j++)
-            {
-               c[i][j]=a[i][j]+b[i][j];
-               System.out.print(c[i][j]+ "  ");
+        for (i = 0; i < row; i++) {
+            for (j = 0; j < col; j++) {
+                System.out.print(a[i][j] + b[i][j] + "  ");
             }
             System.out.println("\n");
         }
-         
-        
+
     }
 }
