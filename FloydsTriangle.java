233972ed9158eb0c6f92d1fb48daf83fe749291e
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-11-06 20:41:46 +0530

    Update FloydsTriangle.java

--- FloydsTriangle.java
+++ FloydsTriangle.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class FloydsTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number: ");
+        System.out.print("Enter the number of rows: ");
         int n = sc.nextInt();
 		int i;
 		int j;
 		int k=1;
-		for (i=0;i<=n;i++)
+		for (i=0;i<n;i++)
 		{
 			for (j=0;j<=i;j++)
 			{
