e52362204951a9ea781e1a7c62c0bef01c65fb62
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-11-06 20:26:15 +0530

    Create ConstructorExample.java

---
+++ ConstructorExample.java
@@ -0,0 +1,14 @@
+public class Triangle {
+    int side;
+
+    public Triangle() // No Paramerter
+    {
+        side = 5;
+    }
+
+    public Triangle(int y) // Parameter yes
+    {
+        side = y;
+    }
+}
+// This is a non executable program file
