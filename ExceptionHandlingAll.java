e8b2806a8ebd24bd6aea4b437b8e0365d3a7bc48
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-11-07 06:21:55 +0530

    Update and rename labs/EXCEPTION HANDLING.java to ExceptionHandlingAll.java

---
+++ ExceptionHandlingAll.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1.ArithmeticException ");
+        int a = 10;
+        int b = 0;
+        try {
+            int x = a / b;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println();
+
+        System.out.println("2.ArrayIndexOutOfBoundsException");
+        int[] arr = new int[10];
+        try {
+            arr[12] = 5;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println();
+
+        System.out.println("3.ClassNotFoundException");
+        try {
+            Class.forName("Class Employee"); // Class Employee is not defined
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println("4.NumberFormatException");
+        try {
+            // "akki" is not a number
+            int num = Integer.parseInt("akki");
+
+            System.out.println(num);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        System.out.println();
+    }
+}
